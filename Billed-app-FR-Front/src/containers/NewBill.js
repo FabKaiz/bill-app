4956e72b17ad9c611fccde041d6ae937232e4902
FabKaiz <cacrfabfr@gmail.com>
2023-07-06 18:01:37 +0200

    [!] - Refactor Bills

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -16,49 +16,40 @@ export default class NewBill {
     new Logout({document, localStorage, onNavigate})
   }
 
-  handleChangeFile = e => {
-    e.preventDefault();
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    const fileExtension = fileName.split('.').pop().toLowerCase();
+handleChangeFile = (e) => {
+  e.preventDefault();
+  const file = e.target.files[0];
+  const fileName = file.name;
+  const fileExtension = fileName.substring(fileName.lastIndexOf("."));
 
-    // Check if the file extension is valid (JPG, JPEG, or PNG)
-    if (!['jpg', 'jpeg', 'png'].includes(fileExtension)) {
-      e.target.value = '';
+  const isValidExtension = [".jpg", ".jpeg", ".png"].includes(fileExtension.toLowerCase());
 
-      const message = document.createElement('p');
-      message.textContent = 'Invalid file format. Please upload a JPG, JPEG, or PNG file.';
-      message.style.color = 'red';
-      message.classList.add('warning-message');
-      e.target.parentNode.insertBefore(message, e.target.nextSibling);
-      return;
-    }
-
-    const warningMessage = document.querySelector('.warning-message');
-    if (warningMessage) warningMessage.remove();
+  if (isValidExtension) {
+    e.target.setCustomValidity("");
 
     const formData = new FormData();
     const email = JSON.parse(localStorage.getItem("user")).email;
-    formData.append('file', file);
-    formData.append('email', email);
+    formData.append("file", file);
+    formData.append("email", email);
 
-    this.store
-      .bills()
+    this.store.bills()
       .create({
         data: formData,
         headers: {
-          noContentType: true
-        }
+          noContentType: true,
+        },
       })
-      .then(({fileUrl, key}) => {
+      .then(({ fileUrl, key }) => {
         console.log(fileUrl);
         this.billId = key;
         this.fileUrl = fileUrl;
         this.fileName = fileName;
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
+  } else {
+    e.target.setCustomValidity("Le format doit être JPG, JPEG ou PNG");
   }
+};
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
